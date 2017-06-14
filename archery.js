094dc5806a11501cf355d20cf0a0a00e4650c8eb
Mathison, Timothy <timothy.mathison@optum.com>
2017-06-14 09:43:19 -0500

    change custom click aws call

--- archery.js
+++ archery.js
@@ -5,11 +5,13 @@ var sendClick = function(event)
     console.log(event);
     var httprequest = new XMLHttpRequest();
     httprequest.withCredentials = true;
+    httprequest.open("GET", "https://ta72cmbb94.execute-api.us-east-2.amazonaws.com/helloworld", true);
+
     httprequest.onreadystatechange = function() {
         if (httprequest.readyState == 4 && httprequest.status == 200)
             alert(httprequest.responseText);
     }
-    httprequest.open("GET", "https://ta72cmbb94.execute-api.us-east-2.amazonaws.com/helloworld", true);
+
     httprequest.send(null)
 
 }
