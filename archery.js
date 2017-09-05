89fd3332c822c606a357ae5021b83d021eccbc7d
Timothy Mathison <timothy.mathison96@gmail.com>
2017-09-05 13:02:23 -0500

    removing get request on click and heatmap script link

--- archery.js
+++ archery.js
@@ -3,15 +3,15 @@
 var sendClick = function(event)
 {
     console.log(event);
-    var httprequest = new XMLHttpRequest();
-    httprequest.open("GET", "https://ta72cmbb94.execute-api.us-east-2.amazonaws.com/helloworld/helloworld?name=tim&title=intern", true);
-
-    httprequest.onreadystatechange = function() {
-        if (httprequest.readyState == 4 && httprequest.status == 200)
-            console.log(httprequest.responseText);
-    }
-
-    httprequest.send();
+    // var httprequest = new XMLHttpRequest();
+    // httprequest.open("GET", "https://ta72cmbb94.execute-api.us-east-2.amazonaws.com/helloworld/helloworld?name=tim&title=intern", true);
+    //
+    // httprequest.onreadystatechange = function() {
+    //     if (httprequest.readyState == 4 && httprequest.status == 200)
+    //         console.log(httprequest.responseText);
+    // }
+    //
+    // httprequest.send();
 
 }
 
