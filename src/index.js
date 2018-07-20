0e631299d0d8c4bbc24d2c7a44d8f577a1fa1e40
Timothy <timothy.mathison96@gmail.com>
2018-07-20 09:41:21 -0500

    test stuff

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import u from './utility.js'
 document.onload = function() {
     console.log("Hello from onload function");
     u.util("I was imported");
-}
+};
 
-// document.addEventListener("DOMContentLoaded", function(){
-//     console.log("Hello from load event listener");
-// });
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Hello from load event listener");
+});
