6939aed9dd06484049e5e808714f4240bf424c79
Timothy <timothy.mathison96@gmail.com>
2018-07-20 19:05:04 -0500

    menue work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import './styles.css'
 import u from './utility.js'
 
+let setUpEvents = () => {
+    let menu = document.getElementsByClassName("menu")[0];
+    if(!menu || menu === null) {
+        setTimeout(setUpEvents, 1000);
+        console.log(menu);
+    } else {
+        let menuLinks = menu.children[menu.childElementCount - 1];
+        menuLinks.addEventListener("mouseover", openDropDown)
+        console.log(menuLinks);
+    }
+};
+
+let openDropDown = () => {
+    let menu = document.getElementsByClassName("menu")[0];
+    let menuLinks = menu.children[menu.childElementCount - 1];
+    let link = document.getElementById("links");
+
+    if(menuLinks.className !== "dark") {
+        menuLinks.className = "dark"
+    }
+
+    // let isHidden = Array.from(link.classList).indexOf("hidden");
+    if(link.classList.contains("hidden")) {
+        link.classList.remove("hidden")
+    }
+};
+
 window.onload = function() {
-    console.log("Hello from onload function");
-    u.util("I was imported");
+    console.log("At onload function");
+    // u.util("I was imported");
+
+    setUpEvents();
 }(u);
